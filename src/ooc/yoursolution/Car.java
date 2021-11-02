e73e99fcfa73a2ab20516412fa3c23ced5d0e960
Ingrid Castro <72034663+IngDih@users.noreply.github.com>
2021-11-02 19:45:26 +0000

    Getters and Setters for make and rate on Car.java

--- src/ooc/yoursolution/Car.java
+++ src/ooc/yoursolution/Car.java
@@ -17,10 +17,13 @@ import ooc.enums.Month;
 public class Car implements CarInterface {
 
     //Variables declaration\\
-
+    private Make make;
+    private double rate;
+    
     //Constructor\\
-    public Car() {
-
+    public Car(Make make, double rate) {
+        this.make = make;
+        this.rate = rate;
     }
 
     @Override
@@ -40,22 +43,22 @@ public class Car implements CarInterface {
 
     @Override
     public Make getMake() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.make;
     }
 
     @Override
     public void setMake(Make make) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.make = make;
     }
 
     @Override
     public double getRate() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.rate;
     }
 
     @Override
     public void setRate(double rate) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.rate = rate;
     }
 
     @Override
