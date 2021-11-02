a7398e22a796731eacf1e6fa92152fc100aa1a67
Robert Szlufik <robert.szlufik123@gmail.com>
2021-11-02 22:15:49 +0000

    bug fixes

--- src/ooc/yoursolution/RentACar.java
+++ src/ooc/yoursolution/RentACar.java
@@ -70,11 +70,53 @@ public class RentACar implements RentACarInterface {
         }
             
     return flag;
-        
     }
     
+//    @Override
+//    public boolean checkAvailability(Month month, int day, Make make, int lengthOfRent) {
+//        boolean testBoolean = true;
+//        boolean flag = false;
+//        int from = day;
+//        int to = from + lengthOfRent;
+//        List<CarInterface> listOfMake = this.getAllCarsOfAMake(make, this.listOfCars);
+//        List<Boolean> dateForID = new ArrayList<>();
+//
+//        for(CarInterface car: listOfMake){
+//            Map<Month, boolean[]> tempMap = car.getAvailability();
+//
+//                for(Map.Entry<Month, boolean[]> entry : tempMap.entrySet()) {
+//                    if(entry.getKey().compareTo(month) == 0) {
+//                        for(int i = from; i < to ; i++) {
+//                            if(entry.getValue()[i] == false) {
+//                                testBoolean = false;
+//                            }
+//                        }
+//                    }
+//                    if(testBoolean == true) {
+//                    dateForID.add(Boolean.TRUE);
+//                    }
+//                }
+//            car.setAvailability(tempMap);
+//                
+//            
+//        }
+//        
+//        
+//        for(Boolean b: dateForID) {
+//            if(b == Boolean.TRUE){
+//                flag = true;
+//            } 
+//        }
+//            
+//            
+//    return flag;
+//        
+//    }
+//    
+        
+    
 
-    @Override
+     @Override
     public int getCarAvailable(Month month, int day, Make make, int lengthOfRent) {
         // retreving list of cars of the same make
         List<CarInterface> listOfMake = this.getAllCarsOfAMake(make, this.listOfCars);
@@ -85,49 +127,26 @@ public class RentACar implements RentACarInterface {
         
         int id = 0; // if -1 is returned, the car is not available 
         
-        int from = day-1;
-        int to = from + lengthOfRent;
-        List<Boolean> dateForID = new ArrayList<>();
+        int from = day;
+        int to = from + lengthOfRent;   
     
         if(isAvailable) {
-            
-            
-            
+  
             for(CarInterface car: listOfMake){
-                
-                // test
-//                Map<Month, boolean[]> tempMap = car.getAvailability();
-//                
-//                for(Map.Entry<Month, boolean[]> entry : tempMap.entrySet()){
-//                    if(entry.getKey().compareTo(month)== 0) {
-//                        for(int i = from; i < to ; i++) {
-//                            if(entry.getValue()[i] == false) {
-//                            //if any of the days is not available, test boolean is false 
-//                            testBoolean = false;
-//                            }
-//                        }
-//                        if(testBoolean == true) {
-//                            System.out.println(car.getId());
-//                            idList.add(car.getId());
-//                        }
-//                    }
-//                }
 
                 boolean[] tempCheckArray = car.getAvailability().get(month);
 
                 for(int i = from; i < to ; i++) {
-                   if(tempCheckArray[i] = false) {
+                   if(!tempCheckArray[i]) {
                        //if any of the days is not available, test boolean is false 
                        testBoolean = false;
                    }
                 }
                 // if all days available 
-                if(testBoolean == true) {
+                if(testBoolean) {
                     idList.add(car.getId());
                 }
             }
-
-
             for(Integer i: idList) {
                 //assigns last available 
                id=i;
@@ -139,17 +158,14 @@ public class RentACar implements RentACarInterface {
         return id;
     }
 
-    @Override
-    public boolean bookCar(Month month, int day, Make make, int lengthOfRent) {
+   public boolean bookCar(Month month, int day, Make make, int lengthOfRent) {
         
         boolean bookingCompleted = false;
         
-        int from = day - 1;
+        int from = day ;
         int to = from + lengthOfRent;
         int carID = this.getCarAvailable(month, day, make, lengthOfRent);
         List<CarInterface> carsOfMake = this.getAllCarsOfAMake(make, listOfCars);
-//        Map<Month,boolean[]> newAvailability; 
-//        boolean[] updatedDays;
         
         if(carID > 0) {
             CarInterface tempCar; 
